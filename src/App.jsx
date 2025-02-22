318592e7cd57d8c379b7b7ad66dd0fee9bbb1c3b
surya <swainsuryakanta97@gmail.com>
2025-02-23 03:50:08 +0530

    update mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // Import dependencies
-import React, { useRef,  useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as cocossd from "@tensorflow-models/coco-ssd"
 import Webcam from "react-webcam";
@@ -8,9 +8,16 @@ import { Dot, LoaderCircleIcon } from "lucide-react";
 // e.g. import { drawRect } from "./utilities";
 
 function App() {
+
+  const [facingMode, setFacingMode] = useState("user");
+
+  const switchCamera = () => {
+    setFacingMode((prevMode) => (prevMode === "user" ? "environment" : "user"));
+  };
+
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
-  const [loading , setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   // Main function
   const runCoco = async () => {
@@ -21,9 +28,9 @@ function App() {
         detect(net);
       }, 100);
     } catch (error) {
-      
+
     }
-    finally{
+    finally {
       setLoading(false)
     }
   };
@@ -55,37 +62,42 @@ function App() {
       const ctx = canvasRef.current.getContext("2d");
 
       // Update drawing utility
-      drawRect(obj,ctx)
+      drawRect(obj, ctx)
     }
   };
 
   useEffect(() => { runCoco() }, []);
 
   return (
-    loading? (
+    loading ? (
       <div className="w-screen h-screen bg-zinc-800 flex flex-col items-center justify-center">
-        <LoaderCircleIcon size={64} className={`animate-spin mb-2 text-emerald-500`}/>
+        <LoaderCircleIcon size={64} className={`animate-spin mb-2 text-emerald-500`} />
         <h2 className="text-white text-2xl md:text-3xl">Model is Loading Please Wait</h2>
       </div>
     ) : (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-[url('/background.jpg')] bg-cover bg-center text-white p-2 relative">
-      <h1 className="text-xl md:text-3xl font-bold mb-4 text-center">REAL-TIME OBJECT DETECTION</h1>
-      <div className="relative w-full max-w-8xl h-auto px-2">
-        <Webcam
-          ref={webcamRef}
-          muted={true}
-          className="w-full h-[90vh] md:h-[85vh] rounded-lg shadow-lg object-cover border-2 border-white"
-          videoConstraints={{
-            facingMode: "user",
-          }}
-        />
-        <canvas
-          ref={canvasRef}
-          className="absolute top-0 left-0 w-full h-full"
-        />
+      <div className="flex flex-col items-center justify-center max-h-screen bg-[url('/background.jpg')] bg-cover bg-center text-white py-2 relative">
+        <h1 className="text-2xl md:text-3xl font-bold mb-4 text-center absolute md:static top-4 z-10">REAL-TIME OBJECT DETECTION</h1>
+        <div className="relative w-full max-w-4xl h-auto">
+          <Webcam
+            ref={webcamRef}
+            muted={true}
+            className="w-full h-screen md:h-[480px] rounded-lg shadow-lg object-cover"
+            videoConstraints={{
+              facingMode: facingMode,
+            }}
+          />
+          <canvas
+            ref={canvasRef}
+            className="absolute top-0 left-0 w-full h-full"
+          />
+        </div>
+        <div className="mt-4 flex gap-4 absolute bottom-5">
+          <button onClick={switchCamera} className="bg-green-500 hover:bg-green-600 md:hidden px-2 rounded-md mb-5">
+            Switch Camera
+          </button>
+        </div>
+        <footer className="absolute bottom-0 m-auto text-white/50">Made with ❤️ by Surya</footer>
       </div>
-      <footer className="absolute bottom-0 m-auto text-white/50">Made with ❤️ by Surya</footer>
-    </div>
     )
   );
 }
